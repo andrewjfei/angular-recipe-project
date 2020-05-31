31c29f2a99b68b2d33e0d9ac50c18958229a5bf1
andrewjfei <andrewjfei@gmail.com>
2020-06-01 00:33:00 +1200

    inner structure of components completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-list/recipe-detail/recipe-detail.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
+    ShoppingEditComponent,
     RecipeListComponent,
-    ShoppingListEditComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeComponent,
-    RecipesComponent,
-    ShoppingEditComponent
+    RecipesComponent
   ],
   imports: [
     BrowserModule
